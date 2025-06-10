d0dbf19e1e8aa5933b6cf892a6266f7f8c172bda
Abrar Zuhaer Tariq <abrar@Abrars-MacBook-Air.local>
2025-06-09 18:15:05 -0700

    area map 1

--- src/components/AreaMap.jsx
+++ src/components/AreaMap.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react';
-// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
-// const mapContainerStyle = {
-//   width: '100%',
-//   height: '300px',
-// };
-
-// const center = {
-//   lat: 23.8103, // Dhaka default
-//   lng: 90.4125,
-// };
-
-// function AreaMap({ location }) {
-//   return (
-//     <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
-//       <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={8}>
-//         <Marker position={center} />
-//       </GoogleMap>
-//     </LoadScript>
-//   );
-// }
-
-// export default AreaMap;
 
 import React from 'react';
 
@@ -45,6 +20,4 @@ function AreaMap({ location }) {
   );
 }
 
-
 export default AreaMap;
-
